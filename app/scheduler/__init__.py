f289e59ae6404f41e42dca9aa9c477adb36e8560
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-10-22 00:45:18 +0800

    減少推播消化時間區間

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -10,8 +10,8 @@ tw = pytz.timezone('Asia/Taipei')
 now = datetime.now(tw)
 sched = BackgroundScheduler(timezone="Asia/Taipei", daemon=True)
 sched.add_job(job_service.demo_job, 'interval', minutes=1)
-POST_QUEUE = MessageQueue()  # 註冊一個推播的queue，每5秒推一則，server才不會爆掉
-sched.add_job(post_service.post_to_line, 'interval', seconds=5, args=[POST_QUEUE])
-ANSWER_QUEUE = MessageQueue()  # 註冊一個回答的queue，每5秒推一則，server才不會爆掉
-sched.add_job(post_service.answer_to_poster, 'interval', seconds=5, args=[ANSWER_QUEUE])
+POST_QUEUE = MessageQueue()  # 註冊一個推播的queue，每1秒推一則，server才不會爆掉
+sched.add_job(post_service.post_to_line, 'interval', seconds=1, args=[POST_QUEUE])
+ANSWER_QUEUE = MessageQueue()  # 註冊一個回答的queue，每1秒推一則，server才不會爆掉 (5秒太慢)
+sched.add_job(post_service.answer_to_poster, 'interval', seconds=1, args=[ANSWER_QUEUE])
 sched.start()
